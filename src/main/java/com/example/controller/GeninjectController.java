2e71aefdf375d560de87d0000aa1412c043fe07b
Beşir AYDEMİR <besir.aydemir@hmb.gov.tr>
2025-08-06 14:09:10 +0300

    injetion metodu ile controller db dem veri çekme sağlandoı

--- src/main/java/com/example/controller/GeninjectController.java
+++ src/main/java/com/example/controller/GeninjectController.java
@@ -7,8 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -17,55 +15,36 @@ import com.example.model.User;
 
 public class GeninjectController extends MultiActionController {
 
-	public ModelAndView hnduruninj(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		
-		// Spring context'i al
-        WebApplicationContext context =  WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+    private UserDao userDao;
 
-        // Spring bean olarak tanımlanmış userDao'yu al
-        UserDao userDao = (UserDao) context.getBean("userDao");
+    // Spring XML'den inject edilecek
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-        // Veritabanından kullanıcı sayısını çek
-        int count = (Integer) userDao.countUsers();
+    public ModelAndView hnduruninj(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		
-		
-		
+        Object count = userDao.countUsers();
 
-		return new ModelAndView("urun");
-	}
+        ModelAndView mav = new ModelAndView("urun");
+        mav.addObject("count", count); // sayıyı modele ekledik (istenirse JSP'de gösterilir)
+        return mav;
+    }
 
-	public ModelAndView hnddenemeinj(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    public ModelAndView hnddenemeinj(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		return new ModelAndView("deneme");
-	}
-	
-	
-	
+        return new ModelAndView("deneme");
+    }
+
     public ModelAndView hndusersinj(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Spring context'i al
-        WebApplicationContext context =   WebApplicationContextUtils.getWebApplicationContext(getServletContext());
-
-        // Spring bean olarak tanımlanmış userDao'yu al
-        UserDao userDao = (UserDao) context.getBean("userDao");
-
-        // Tüm kullanıcıları listele
         List<User> users = userDao.getAllUsers();
 
-        // ModelAndView oluştur ve kullanıcıları modele ekle
-        ModelAndView mav = new ModelAndView("users"); // urun.jsp
+        ModelAndView mav = new ModelAndView("users");
         mav.addObject("users", users);
-
         return mav;
     }
-	
-	
-	
-	
-
 }
